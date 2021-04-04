f2f88f0779e99f0179cf94f9a96602c60a6aa83c
MasuqaT <occar@hotmail.co.jp>
2021-04-04 17:07:41 +0900

    format echo with rustfmt

--- chapter-1/src/echo.rs
+++ chapter-1/src/echo.rs
@@ -1,6 +1,6 @@
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+use std::net::SocketAddr;
 
 async fn handler(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     println!("{:#?}", req);
@@ -20,9 +20,7 @@ async fn route(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, hyper::Error>(service_fn(route))
-    });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, hyper::Error>(service_fn(route)) });
 
     println!("start http listening :18888");
 
